d631de163b061c071eba1d0940734b16f098ea0e
pepik_knize <joeknize@yahoo.com>
2025-08-13 23:13:01 -0500

    fixed it

--- rtl_433.sh
+++ rtl_433.sh
@@ -5,5 +5,5 @@ export set MQTT_PASSWORD=$MQTT_PASSWORD
 date
 echo starting up
 echo username: $MQTT_USERNAME
-# rtl_433 -F json -M utc -M level -M protocol -f 915M -f 433.92M -H 120 -Y classic -F mqtt://hass.knize.org,retain=0
+# rtl_433 -F json -M utc -M level -M protocol -f 915M -f 433.92M -H 120 -Y classic -F mqtt://192.168.0.181,retain=0
 rtl_433 -c /home/joe/git/rtl_433/conf/rtl_433.conf
