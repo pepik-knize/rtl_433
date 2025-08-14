aa98ed9ee41700034d4896409f1d05ca9997bb1b
pepik_knize <joeknize@yahoo.com>
2025-08-13 22:53:03 -0500

    Adding shell script and conf

--- rtl_433.sh
+++ rtl_433.sh
@@ -2,7 +2,8 @@
 source /home/joe/secrets/secrets.sh
 export set MQTT_USERNAME=$MQTT_USERNAME
 export set MQTT_PASSWORD=$MQTT_PASSWORD
+date
+echo starting up
 echo username: $MQTT_USERNAME
-rtl_433 -F json -M utc -M level -M protocol -f 915M -f 915M -f 915M -f 915M -f 433.92M -H 60 -Y classic -F mqtt://hass.knize.org,retain=0
-# rtl_433 -F json -M utc -M level -M protocol -f 915M -Y classic -F mqtt://hass.knize.org,retain=0
-# rtl_433 -c /home/joe/git/rtl_433/conf/rtl_433.conf
+# rtl_433 -F json -M utc -M level -M protocol -f 915M -f 433.92M -H 120 -Y classic -F mqtt://hass.knize.org,retain=0
+rtl_433 -c /home/joe/git/rtl_433/conf/rtl_433.conf
